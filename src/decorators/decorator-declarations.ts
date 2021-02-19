3656566113ef59733a1c8585e89a8757eb9a840d
XeroxDev <admin@xeroxdev.de>
2021-02-19 18:32:29 +0100

    Added types

---
+++ src/decorators/decorator-declarations.ts
@@ -0,0 +1,8 @@
+import { SDDebug } from "./classes/debug.decorator";
+import { SDPlugin } from "./classes/plugin.decorator";
+import { SDPropertyInspector } from "./classes/property-inspector.decorator";
+import { SDInit } from "./methods/init.decorator";
+import { SDOnEvent } from "./methods/on-event.decorator";
+import { SDReady } from "./methods/ready.decorator";
+
+export {SDDebug, SDPlugin, SDPropertyInspector, SDInit, SDOnEvent, SDReady}
