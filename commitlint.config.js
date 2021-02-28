68a8609fcf2edb61f289e0c78f8f1c38f539b7be
XeroxDev <admin@xeroxdev.de>
2021-02-28 20:03:48 +0100

    CI: changed commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-"module.exports = {extends: ['@commitlint/config-conventional']};" 
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-case': [2, 'always', 'upper-case']
+    },
+};
