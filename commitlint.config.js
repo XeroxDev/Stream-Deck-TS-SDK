a2b90e48c30b0c0a5e21ab1625ea79905949de47
XeroxDev <admin@xeroxdev.de>
2021-02-28 20:59:44 +0100

    ci: downgraded to husky 3

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-case': [2, 'always', 'upper-case']
-    },
-};
+module.exports = {extends: ['@commitlint/config-conventional']};
